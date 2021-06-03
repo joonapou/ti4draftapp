f4c6b83b85f2ed490a7204b490dd7d4973e0f3e0
Joona Poutanen <joona.poutanen@solita.fi>
2021-06-03 14:28:52 +0300

    added pickorder logic

---
+++ pages/api/pickorder.js
@@ -0,0 +1,29 @@
+import prisma from '../../lib/prisma'
+
+
+export default async function handle(req, res) {
+ const users = await prisma.user.findMany();
+  const user_array = Object.values(users);
+  var users_asc = [];
+  var users_desc = [];
+  var i;
+  for (i = 0; i < user_array.length; i++){
+    users_desc.push([user_array[i].pickOrder, user_array[i].name])
+    users_desc.sort(function compare(user1, user2) {
+      return user1.[0]-user2.[0]
+    }
+      )
+  }
+  for (i = 0; i < user_array.length; i++){
+    users_asc.push([user_array[i].pickOrder, user_array[i].name])
+    users_asc.sort(function compare(user1, user2) {
+      return user2.[0]-user1.[0]
+    }
+      )
+  }
+  var sortable_users = users_asc.concat(users_desc);
+  var sortable_users_obj = {};
+    sortable_users_obj["pick_order"] = sortable_users
+
+  res.status(200).json(sortable_users_obj)
+}
